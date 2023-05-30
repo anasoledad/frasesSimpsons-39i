a6cb02cff47abf30afb79e5ef7f537014e693455
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-30 17:35:33 -0300

    Se agregó el fondo y la imagen del personaje

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -6,7 +6,7 @@ const Frase = () => {
     <section className="my-5">
       <h2>Personaje</h2>
       <hr />
-      <img src="" alt="" />
+      <img className="mb-3" src="https://cdn.glitch.me/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png" alt="" />
       <Card>
         <Card.Body>
           <figure>
