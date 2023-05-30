dff9a8edf4587264fc94e691f7932f043f2dd66f
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-30 18:50:09 -0300

    Se agregó la consulta de la api desde el botón

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './assets/logosimpson.png';
 import { Container, Button } from 'react-bootstrap';
 import Frase from './components/Frase';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
 
+  useEffect(()=>{
+    consultarAPI();
+  }, [])
+
+  const consultarAPI = async ()=>{
+      //realizar una peticion get
+      //https://thesimpsonsquoteapi.glitch.me/quotes
+     try{
+      const respuesta = await fetch ('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const dato = await respuesta.json()
+      console.log(respuesta);
+      console.log(dato[0]);
+      setPersonaje(dato[0])
+     }catch(error){
+      console.log(error)
+     }
+  };
   return (
     <>
     <Container className='text-center my-5'>
         <img src={logo} alt='Logo de los simpsons' className='w-50' /> 
-        <Frase></Frase>
-        <Button variant="warning">Obtener frase</Button>
+        <Frase personaje={personaje}></Frase>
+        <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
     </Container>
    
       
